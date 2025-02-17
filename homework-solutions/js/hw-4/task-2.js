6c0176f40269f3268050658b2848049f79cb1de4
Aleksey Naumov <naumov.alex00@icloud.com>
2025-02-17 21:51:25 +0300

    Update homework-solutions/js/hw-4/task-2.js
    Co-authored-by: Volodymyr Lazurenko <103571439+VladimirLQA@users.noreply.github.com>

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -19,38 +19,21 @@ const minAge = 18;
 const maxAge = 60;
   
   let age = 10;
-  const typeOfAge = Number(age);
+const age = 10;
 
-if (age < minAge ) 
-{ 
-  if (typeof typeOfAge !== typeof age) 
-  {
-  console.log('Incorrect data type')
-  }
-  console.log("You don't have access cause your age is " + age + " It's less then 18")
-}
-else if (age >= minAge && age < maxAge) 
-{
-  if (typeof typeOfAge !== typeof age) 
-  {
+if (typeof age !== 'number') {
   console.log('Incorrect data type');
-  }
-  console.log("Welcome !")
+} else { 
+   if (age < minAge) {
+      console.log(
+    "You don't have access cause your age is " + age + " It's less then 18",
+  );
+} else if (age >= minAge && age < maxAge) {
+  console.log('Welcome !');
+} else if (age >= maxAge) {
+  console.log('Keep calm and look Culture channel');
+} else {
+  console.log('Technical work');
 }
-else if (age >= maxAge) 
-{
-  if (typeof typeOfAge !== typeof age) 
-  {
-  console.log('Incorrect data type')
-  }
-  console.log("Keep calm and look Culture channel")
-} 
-else {
-  if (typeof typeOfAge !== typeof age) 
-   {
-    console.log('Incorrect data type')
-  }
-  console.log("Technical work")
-};
 
  
