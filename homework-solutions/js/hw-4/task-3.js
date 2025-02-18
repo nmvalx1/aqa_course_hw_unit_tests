46993c211d1ea8f02c3dd753b0c5059939e6d144
Aleksey Naumov <naumov.alex00@icloud.com>
2025-02-18 23:07:52 +0300

    Update task-3.js

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -9,38 +9,21 @@
 const minAge = 18;
 const maxAge = 60;
   
-  let age = '61';
-  const typeOfAge = Number(age);
+let age = '18'
 
-if (age < minAge ) 
-  { 
-    if (typeof typeOfAge !== typeof age) 
-  {
-    +age
-  }
-  console.log("You don't have access cause your age is " + age + " It's less then 18")
+if (age && isNaN(age))  {
+  console.log('Incorrect data type')
 }
-  else if (age >= minAge && age < maxAge) 
-{
-  if (typeof typeOfAge !== typeof age) 
-  {
-    +age
-  }
-  console.log("Welcome !")
+else { age = Number(age);
+  if(age<minAge) {
+  console.log("You don't have access cause your age is " + age + " It's less then ")
+} else if (age >= minAge && age < maxAge) {
+  console.log('Welcome !');
+} else if (age >= maxAge) {
+  console.log('Keep calm and look Culture channel');
+} else {
+  console.log('Technical work');
 }
-else if (age >= maxAge) 
-{
-   if (typeof typeOfAge !== typeof age) 
-  {
-    +age
-   }
-   console.log("Keep calm and look Culture channel")
-} 
-else
-{
-  if (typeof typeOfAge !== typeof age) 
-  {
-    console.log('Incorrect data type')
-  }
-  console.log("Technical work")
-};
+}
+
+console.log(typeof age)
