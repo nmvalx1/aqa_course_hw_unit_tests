1e151d06cecbedb33d55dd6a910e2bbd24a0e91a
Aleksey Naumov <naumov.alex00@icloud.com>
2025-03-09 17:30:35 +0300

    hw7

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -6,6 +6,17 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique;
+const arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
+let b =[]
+const unique = [];
+
+for(let i=0;i<arrNumbers.length;i++){
+
+if(arrNumbers[i].includes(arrNumbers[i])){
+ unique.push(arrNumbers[i])
+}
+}
+
+console.log(b)
 
 export { unique };
