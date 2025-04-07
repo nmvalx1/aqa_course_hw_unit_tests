3254d0a4ea1b60b8696bf5feb5e1e06f80801d1c
Aleksey Naumov <naumov.alex00@icloud.com>
2025-04-07 19:47:40 +0300

    Update task-2.ts

--- homework-solutions/ts/task-2.ts
+++ homework-solutions/ts/task-2.ts
@@ -8,31 +8,39 @@
 // Функция должна возвращать false, если хотя бы одно из условий не выполнено.
 
 enum LETTERS_AND_NUMBERS_OF_PASS {
-  lower = 'abcdefghijklmnopqrstuvwxyz',
-  upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number = '1234567890',
+  LOWER= 'abcdefghijklmnopqrstuvwxyz',
+  UPPER= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  NUMBER= '1234567890',
 }
 
+// function validatePassword(pass:string): boolean {
+//   let result: boolean;
+//   // pass.trim();
+//   const splittedpass: string[] = pass.split('');
+//   if (!(splittedpass.length > 8)) {
+//     return result = false;
+//   }
+//   if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.upper.split('').includes(el))) {
+//     return result = false;
+//   }
+//   if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.lower.split('').includes(el))) {
+//     return result = false;
+//   }
+//   if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.number.split('').includes(el))) {
+//     return result = false;
+//   }
+//   if (splittedpass.some((el) => ' '.includes(el))) {
+//     return result = false;
+//   }
+//   return result = true;
+// }
+
+// console.log(validatePassword('AlekseyNaumov1'));
 function validatePassword(pass:string): boolean {
-  let result: boolean;
-  // pass.trim();
-  const splittedpass: string[] = pass.split('');
-  if (!(splittedpass.length > 8)) {
-    return result = false;
-  }
-  if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.upper.split('').includes(el))) {
-    return result = false;
-  }
-  if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.lower.split('').includes(el))) {
-    return result = false;
-  }
-  if (!splittedpass.some((el) => LETTERS_AND_NUMBERS_OF_PASS.number.split('').includes(el))) {
-    return result = false;
-  }
-  if (splittedpass.some((el) => ' '.includes(el))) {
-    return result = false;
-  }
-  return result = true;
+  const trimmed = pass.trim();
+  if (trimmed.length <= 8) return false;
+  return [LETTERS_AND_NUMBERS_OF_PASS.UPPER, LETTERS_AND_NUMBERS_OF_PASS.LOWER, LETTERS_AND_NUMBERS_OF_PASS.NUMBER].every((pattern) =>
+      [...trimmed].some((char) => pattern.includes(char))
+  );
 }
-
-console.log(validatePassword('AlekseyNaumov1'));
+console.log(validatePassword('AlekseyNaumov'));
